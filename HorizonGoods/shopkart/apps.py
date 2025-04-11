9d3495d3a7cda93702cd7834229048420a4a695b
Udhaymastro <69453541+Udhaymastro@users.noreply.github.com>
2025-04-11 22:36:48 +0530

    Delete HorizonGoods directory

--- HorizonGoods/shopkart/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopkartConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shopkart'
--- HorizonGoods/shopkart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from shopkart.views import login
-
-urlpatterns = [
-    path('',login.loginform,name='loginform'),
-    path('registerform/',login.registerform,name='registerform'),
-    path('signin/',login.signin,name='signin'),
-    path('signup/',login.signup,name='signup'),
-]
